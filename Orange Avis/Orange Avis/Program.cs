965d715375a193f56b1b77aba2f766a2660ac591
KillerBies <johannescj@gmail.com>
2025-03-18 10:33:47 +0100

    Add files via upload

---
+++ Orange Avis/Orange Avis/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Orange_Avis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string produktNavn = "Loftlampe";
+            string produktKategori = "Belysning";
+            double produktNyPris = 2500;
+            double produktPris = 1000;
+            string produktStand = "God";
+            Console.WriteLine(FindBesparelse(produktPris, produktNyPris));
+        }
+        static int FindBesparelse(double pris, double nyPris)
+        {
+            double doubleBesparelse = 100 - ((pris / nyPris) * 100);
+            int besparelse = Convert.ToInt32((double)doubleBesparelse);
+            return besparelse;
+        }
+    }
+}
